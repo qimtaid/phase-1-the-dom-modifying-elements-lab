80754a4f11be077af51259f20262f4c758198be1
Dan Koskei <qimtaid@gmail.com>
2024-03-31 23:09:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// Remove the main element 
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create element
+const newHeader = document.createElement('h1');
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME 
+newHeader.id = "victory";
+
+// Append the new h1 element t
+document.body.append(newHeader);
